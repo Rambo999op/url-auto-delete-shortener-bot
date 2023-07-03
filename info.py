fda217c3ba52e3a26d0ffedc2de2a47cb07c458b
Rambo999op <111131261+Rambo999op@users.noreply.github.com>
2023-07-03 22:14:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,7 +32,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Rex66:Rex66@cluster0.t9mkn2h.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://arijitghorui1234:RaMbO999@cluster0.8xeod2y.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
